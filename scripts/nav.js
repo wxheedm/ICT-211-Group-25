52fa756b8335ff32069f30547c64ff29212b8111
wxheedm <waheedmohammedofficial14@gmail.com>
2025-07-03 23:31:56 -0800

    Delete scripts directory

--- scripts/nav.js
+++
@@ -1,9 +0,0 @@
-let phn = document.getElementById('phn');
-let link2 = document.getElementById('link2');
-
-phn.addEventListener('touchend', function () {
-    link2.style.display = "flex";
-});
-window.addEventListener('scroll', function () {
-    link2.style.display = "none";
-});
